63de6ce80c001867fa3fea8cfdcb30480c58f5c5
dushimepaul-web <dushimepaul51@gmail.com>
2026-06-29 11:09:21 +0200

    Fix: retour sessions en mode files (table sessions incompatible avec CI3) - sess_driver revient a 'files' car la table 'sessions' existante a un schema personnalise - sess_save_path a NULL pour utiliser le repertoire par defaut - stricton remis a FALSE pour compatibilite

--- application/config/config.php
+++ application/config/config.php
@@ -378,10 +378,10 @@ $config['encryption_key'] = 'f7c9e2a4b8d1f3e5a7c9b2d4f6e8a0c3b5d7e9f1a2b4c6d8e0f
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'database';
+$config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = 'sessions';
+$config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
